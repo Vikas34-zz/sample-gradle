fee2aaf0fdb1079077a94ad830cb94dec382801c
Vikas Khinchi <vikas.khinchi@diconium.com>
2020-07-19 16:25:03 +0200

    build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -9,4 +9,5 @@ cd sample-gradle
   -Dsonar.host.url=https://devstack.vwgroup.com/sonar \
   -Dsonar.login=62cebc63d4b0ccab22da7459c32d49452e593ed7
 
+cp ./build/libs/rest-service-0.0.1-SNAPSHOT.jar ../sonarqube-analysis-input/latest.jar
 #cp ./build/libs/rest-service-0.0.1-SNAPSHOT.jar ../sonarqube-analysis-input/latest.jar
