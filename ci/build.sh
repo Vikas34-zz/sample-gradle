b60a6979fbe4fc4cd6b0d69476705dd8a7b0d480
Vikas Khinchi <vikas.khinchi@diconium.com>
2020-07-20 16:07:07 +0200

    build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -2,13 +2,15 @@
 
 set -ex
 cd sample-gradle
-#./gradlew build test
 
-$sonar_token
-#./gradlew sonarqube \
-#  -Dsonar.projectKey=test \
-#  -Dsonar.host.url=https://devstack.vwgroup.com/sonar \
-#  -Dsonar.login=62cebc63d4b0ccab22da7459c32d49452e593ed7
+./gradlew build
+
+echo "Printing sonar token: " $sonar_token
+
+./gradlew sonarqube \
+  -Dsonar.projectKey=test \
+  -Dsonar.host.url=https://devstack.vwgroup.com/sonar \
+  -Dsonar.login=$sonar_token
 
 #cp ./build/libs/rest-service-0.0.1-SNAPSHOT.jar ../sonarqube-analysis-input/latest.jar
 #cp ./build/libs/rest-service-0.0.1-SNAPSHOT.jar ../sonarqube-analysis-input/latest.jar
